05faa00a5353cebefd9ed422479c3f889fb8ab5b
nibral <nibral@dancingstarson.me>
2015-12-29 10:06:51 +0900

    レスポンスの形式と表示精度を変更

--- app.js
+++ app.js
@@ -2,20 +2,28 @@
 
 var express = require('express');
 var app = express();
-
 var exec = require('child_process').exec;
+
+// CPU温度計測コマンド
 const coretempCmd = 'cat /sys/class/thermal/thermal_zone0/temp';
 
 app.get('/', (req, res) => {
   let child = exec(coretempCmd, (err, stdout, stderr) => {
-    let temp = stdout / 1000;
-    res.send(temp + ' °C');
+    // 小数点以下1桁で四捨五入する
+    let temp = stdout / 100;
+    temp = Math.round(temp);
+    temp /= 10;
+    
+    // JSONで返事
+    res.json({ temp: temp });
   });
 });
 
+// 特に指定がなければ3000番ポートで待ち受け
 var server = app.listen(process.env.PORT || 3000, () => {
   let host = server.address().address;
   let port = server.address().port;
 
   console.log('listening at http://%s:%s', host, port);
 });
+
